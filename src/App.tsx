627f5dfd3ad44bbb8c35b38369c8d940cd0ae3a3
Pradeep Sekar <pradeep87sekar@gmail.com>
2022-03-19 17:35:26 +1100

    Create ancient artifacts selector and viewer
    - Removed the custom `<Page />` component that was causing scroll issues on mobile devices.
    
    - Re-enabled scroll on the root `html` and `body` elements.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import Page from 'components/Page';
 import GameInfo from 'containers/GameInfo';
 import IntroHero from 'containers/IntroHero';
 import { HeroMode } from 'containers/IntroHero/types';
@@ -8,13 +7,11 @@ import styles from './App.module.scss';
 
 function App() {
   return (
-    <Page>
-      <main className={styles.App}>
-        <IntroHero mode={HeroMode.Stats} delay={1.5} />
-        <GameInfo delay={2.0} />
-        <TeamInfo delay={2.5} />
-      </main>
-    </Page>
+    <main className={styles.App}>
+      <IntroHero mode={HeroMode.Stats} delay={1.5} />
+      <GameInfo delay={2.0} />
+      <TeamInfo delay={2.5} />
+    </main>
   );
 }
 
